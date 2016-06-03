7f4a9e99dd0352d4ff38c3966fc646d7d9ab7450
JR Rickerson <jrrickerson@gmail.com>
2016-06-02 20:20:14 -0400

    Implement randomized trees and basic player input.

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
+import random
+
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import (
+    NumericProperty, ReferenceListProperty, ObjectProperty)
 from kivy.clock import Clock
 from kivy.vector import Vector
 
 
 class ScrollerGame(Widget):
+    player = ObjectProperty(None)
+    tree1 = ObjectProperty(None)
+    tree2 = ObjectProperty(None)
+    tree3 = ObjectProperty(None)
+
     def update(self, dt):
-        pass
+        self.player.move(dt)
+        self.tree1.scroll(self.player.velocity_x)
+        self.tree2.scroll(self.player.velocity_x)
+        self.tree3.scroll(self.player.velocity_x)
+
+    def on_touch_down(self, touch):
+        if touch.x > self.player.center_x:
+            self.player.velocity_x = 2
+
+    def on_touch_up(self, touch):
+        self.player.velocity_x = 0
 
 
 class RectangleMan(Widget):
+    MAX_VELOCITY_X = 10
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+    def move(self, dt):
+        if self.velocity_x > 0:
+            self.velocity_x += 2 * dt
+        if self.velocity_x > self.MAX_VELOCITY_X:
+            self.velocity_x = self.MAX_VELOCITY_X
+        # Track our velocity but don't actually move the player sprite
+        #self.pos = Vector(*self.velocity) + self.pos
+
+
+class RectangleTree(Widget):
+    def scroll(self, scroll_dist):
+        if self.pos[0] > 0:
+            self.pos = Vector(-1 * scroll_dist, 0) + self.pos
+        else:
+            self.pos = Vector(
+                self.parent.width + random.randint(0, self.parent.width), 0)
 
 
 class ScrollerApp(App):
